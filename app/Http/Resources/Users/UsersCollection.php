7741d5796e8fa523d24926a86a0f55d2984e14e4
saneglos005 <saneglos005@gmail.com>
2024-12-13 03:31:42 +0000

    show up salary in user

--- app/Http/Resources/Users/UsersCollection.php
+++ app/Http/Resources/Users/UsersCollection.php
@@ -26,6 +26,9 @@ class UsersCollection extends ResourceCollection
                     'name' => $user->divisi->name ?? null,
                     'kode_divisi' => $user->divisi->kode_divisi ?? null,
                 ],
+                'daily_salary' => $user->salary ? $user->salary->daily_salary : 0,
+                'hourly_salary' => $user->salary ? $user->salary->hourly_salary : 0,
+                'hourly_overtime_salary' => $user->salary ? $user->salary->hourly_overtime_salary : 0,
                 'created_at' => $user->created_at,
                 'updated_at' => $user->updated_at,
             ];
